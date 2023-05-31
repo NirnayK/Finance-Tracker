0956ff107a7f9059033908344d14e8a673d6e876
nirnay <nirnaykorde@gmail.com>
2023-05-31 23:45:46 +0530

    Frontend Start

--- finance-tracker/components/HomePage.js
+++ finance-tracker/components/HomePage.js
@@ -1,36 +1,16 @@
-
+'use client'
 export default function HomePage() {
     return (
-        //A div saying Your finance tracking issues end here with a  green login button and a green signup button  and gap between them using tailwind css with  sm,md,lg,xl prefix for responsiveness
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-                <h1 className="text-6xl font-bold">
-                    Your finance tracking issues end here
-                </h1>
-
-                <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-                    <a
-                        href="/login"
-                        className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-                    >
-                        <h3 className="text-2xl font-bold">Login &rarr;</h3>
-                        <p className="mt-4 text-xl">
-                            Already have an account? Login here.
-                        </p>
-                    </a>
-                    <a
-                        href="/signup"
-                        className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-                    >
-                        <h3 className="text-2xl font-bold">Sign up &rarr;</h3>
-                        <p className="mt-4 text-xl">
-                            Don't have an account? Sign up here.
-                        </p>
-                    </a>
-                </div>
-            </main>
-        </div>
-
-
+        <section className='w-full flex-center flex-col'>
+            <h1 className='head_text text-center'>
+                Discover & Share
+                <br className='max-md:hidden' />
+                <span className='orange_gradient text-center'> AI-Powered Prompts</span>
+            </h1>
+            <p className='desc text-center'>
+                Promptopia is an open-source AI prompting tool for modern world to
+                discover, create and share creative prompts
+            </p>
+        </section>
     )
 }
