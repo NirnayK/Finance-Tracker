a1872c07aff85d0e22aa5482b34dc2c2913d774d
nirnay <nirnaykorde@gmail.com>
2023-06-05 19:01:41 +0530

    Reformed pages

--- finance-tracker/app/page.js
+++ finance-tracker/app/page.js
@@ -1,14 +1,12 @@
 import Image from "next/image";
 import HomePage from "@/components/HomePage";
-import Dashboard from "@/pages/dashboard";
 import Navbar from "@/components/NavBar";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <Dashboard />
-      {/* <HomePage /> */}
+      <HomePage />
     </>
   );
 }
