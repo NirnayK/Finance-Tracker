1fb322c1c5695336bff9b904e8e15001e3943577
Vatsal Mehta <vatsalm707@gmail.com>
2023-06-05 20:56:50 +0530

    Connected backend for adding expenses

--- finance-tracker/app/api/auth/[...nextauth]/route.js
+++ finance-tracker/app/api/auth/[...nextauth]/route.js
@@ -5,8 +5,6 @@ import GoogleProvider from 'next-auth/providers/google';
 import SequelizeAdapter from "@next-auth/sequelize-adapter";
 import { Sequelize } from "sequelize";
 
-console.log(process.env.GOOGLE_ID);
-
 const sequelize = new Sequelize("postgres://citus:LNMimadY20@c.sga556cc75308043abbbd4cadbb5ac6027.postgres.database.azure.com:5432/citus?sslmode=require", {
     dialectModule: pg
 });
