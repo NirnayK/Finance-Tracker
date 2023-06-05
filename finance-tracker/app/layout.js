f880631c432a6f3b05183fb9c5eaa2284fd1b2c0
Vatsal Mehta <vatsalm707@gmail.com>
2023-06-05 20:31:31 +0530

    Connected backend for adding expenses

--- finance-tracker/app/layout.js
+++ finance-tracker/app/layout.js
@@ -1,20 +1,27 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import {Inter} from 'next/font/google'
 import CustomSessionProvider from '@/components/CustomSessionProvider'
+import {QueryClient, QueryClientProvider} from "react-query";
+import Navbar from "@/components/NavBar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export const metadata = {
   title: 'Finance Tracker',
   description: 'A finance tracker app built with Next.js ',
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout({children}) {
   return (
     <html lang="en" className="bg-white">
-      <CustomSessionProvider>
-        <body className={inter.className}>{children}</body>
-      </CustomSessionProvider>
+    <body className={inter.className}>
+    <CustomSessionProvider>
+      <QueryClientProvider client={new QueryClient()}>
+        <Navbar/>
+        {children}
+      </QueryClientProvider>
+    </CustomSessionProvider>
+    </body>
     </html>
   )
 }
