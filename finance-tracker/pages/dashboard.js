274c2c4ed2f1cb5b2ef57ddfcb4686c6509f16a3
nirnay <nirnaykorde@gmail.com>
2023-06-05 20:07:00 +0530

    shit 4

--- finance-tracker/pages/dashboard.js
+++ finance-tracker/pages/dashboard.js
@@ -4,24 +4,30 @@ import Add from "../components/add";
 import Show from "../components/show";
 import Export from "../components/export";
 import Charts from "../components/charts";
+import Modals from "../components/modals";
 import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import Navbar from '@/components/NavBar';
 import CustomSessionProvider from '@/components/CustomSessionProvider';
+
+const query = new QueryClient();
 export default function Dashboard() {
   //   dashboard page to a financial tracker app using tailwindcss with option to add delete and update the finances
 
   // a react form component using tightwindcss
 
   const [data, setData] = React.useState([]);
+  const [show, setShow] = React.useState(false);
 
   return (
     <>
       <CustomSessionProvider>
-        <Navbar />
-        <Add data={data} set={() => setData(data)} />
-        <Charts data={data} />
-        <Show data={data} />
-        <Export data={data} />
+        <QueryClientProvider client={query}>
+          <Add data={data} set={(d) => setData(d)} />
+          <Charts data={data} />
+          <Show data={data} />
+          <Export data={data} />
+        </QueryClientProvider>
       </CustomSessionProvider>
     </>
   );
