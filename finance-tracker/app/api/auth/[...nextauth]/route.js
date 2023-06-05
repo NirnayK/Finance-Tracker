6ebfa47230093ebcf6d28358171a979ea020891d
nirnay <nirnaykorde@gmail.com>
2023-06-05 20:07:00 +0530

    shit 4

--- finance-tracker/app/api/auth/[...nextauth]/route.js
+++ finance-tracker/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,9 @@
 import NextAuth from "next-auth"
 import GoogleProvider from 'next-auth/providers/google';
+// import SequelizeAdapter from "@next-auth/sequelize-adapter";
+// import { Sequelize } from "sequelize";
+
+// const sequelize = new Sequelize("postgres://citus:LNMimadY20@c.sga556cc75308043abbbd4cadbb5ac6027.postgres.database.azure.com:5432/citus?sslmode=require");
 
 const handler = NextAuth({
     providers: [
@@ -8,6 +12,7 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_SECRET
         }),
     ],
+    // adapter: SequelizeAdapter(sequelize)
 })
 
 export { handler as GET, handler as POST }
--- finance-tracker/components/SignInButton.js
+++ finance-tracker/components/SignInButton.js
@@ -1,15 +1,12 @@
 'use client';
 import { useSession, signIn, signOut } from "next-auth/react"
-import { useRouter } from 'next/router'
 
 const SignInButton = () => {
-
-    // const router = useRouter();
     const { data: session } = useSession();
     if (session) {
         return (
             <>
-                <button className="bg-gray-500 hover:bg-gray-600 rounded-md px-4 py-2 text-white" onClick={() => signOut()}>Log Out</button>
+                <button className="bg-gray-500 hover:bg-gray-600 rounded-md px-4 py-2 text-white" onClick={() => signOut({ callbackUrl: "/" })}>Log Out</button>
             </>
         )
     }
